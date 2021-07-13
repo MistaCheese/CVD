cec8d3fdfa51f0467869ad9baf8f4619977b6b26
MistaCheese <tylimovalex@gmail.com>
2021-07-13 17:55:24 +0300

    первая версия 4

--- src/test/kotlin/checkLocalChromeVersion.kt
+++ src/test/kotlin/checkLocalChromeVersion.kt
@@ -3,29 +3,31 @@ import java.io.File
 import java.lang.Exception
 import java.util.*
 import kotlin.collections.ArrayList
+class CheckLocal {
+    var link: String = CVD().getLocalPathToChromeBrowser()
 
-var link: String = CVD().getLocalPathToChromeBrowser()
 
-
-fun getLastLocalVersion(): String {
-    return checkLocalVersions(link).toString()
-}
-
-fun checkLocalVersions(link: String): String { // Получение версии из локального хранилища в корне хрома
-    val versionSet: ArrayList<String> = ArrayList()
-    val text = File(link)
-    try {
-        for (s1: Int in text.listFiles().indices) {
-            var s2: String = text.listFiles()[s1].toString()
-            if (!s2.contains("Dictionaries") && text.listFiles()[s1].isDirectory && !s2.contains("SetupMetrics")) {
-                versionSet.add(s2.split("\\").last().split(".").first())
-                return versionSet[0]
-            }
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
-        println("Предупреждение!!! Не удалось получить локальнкую версию хрома")
+    fun getLastLocalVersion(): String {
+        return checkLocalVersions(link).toString()
     }
-    return versionSet[0]
+
+    fun checkLocalVersions(link: String): String { // Получение версии из локального хранилища в корне хрома
+        val versionSet: ArrayList<String> = ArrayList()
+        val text = File(link)
+        try {
+            for (s1: Int in text.listFiles().indices) {
+                var s2: String = text.listFiles()[s1].toString()
+                if (!s2.contains("Dictionaries") && text.listFiles()[s1].isDirectory && !s2.contains("SetupMetrics")) {
+                    versionSet.add(s2.split("\\").last().split(".").first())
+                    return versionSet[0]
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println("Предупреждение!!! Не удалось получить локальнкую версию хрома")
+        }
+        return versionSet[0]
+    }
 }
+
 
